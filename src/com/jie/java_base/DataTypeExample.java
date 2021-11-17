0b150cf08e9646afde3b294e1f89f8a8c8b75d8e
lin12.he <hexiaogouzhoujie@gmail.com>
2021-11-17 18:22:50 +0800

    data type cache pool of data type

--- src/com/jie/java_base/DataTypeExample.java
+++ src/com/jie/java_base/DataTypeExample.java
@@ -3,9 +3,11 @@ package com.jie.java_base;
 public class DataTypeExample {
     public static void main(String[] args) {
         baseDataType();
+        dataTypeCachePool();
+        charCachePool();
     }
 
-    private static void baseDataType(){
+    private static void baseDataType() {
         System.out.println(Byte.SIZE);
         System.out.println(Character.SIZE);
         System.out.println(Short.SIZE);
@@ -14,5 +16,26 @@ public class DataTypeExample {
         System.out.println(Long.SIZE);
         System.out.println(Double.SIZE);
         System.out.println(Boolean.FALSE);
+    }
+
+    private static void dataTypeCachePool() {
+        Integer x = new Integer(123);
+        Integer y = new Integer(123);
+        System.out.println(x == y);
+
+        Integer z = Integer.valueOf(123);
+        Integer k = Integer.valueOf("123");
+        System.out.println(z == k);
+
+        Integer m = 123;
+        Integer n = 123;
+        System.out.println(m == n);
+    }
+
+    private static void charCachePool() {
+        for (int i = Integer.parseInt("0000", 16); i <= Integer.parseInt("007F", 16); i++) {
+            char temp = (char) i;
+            System.out.println(temp);
+        }
     }
 }
