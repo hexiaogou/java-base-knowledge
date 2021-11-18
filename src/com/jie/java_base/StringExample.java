f9728b9ccc81aaee12e7c7664c4f48757be4cc28
lin12.he <hexiaogouzhoujie@gmail.com>
2021-11-18 09:45:21 +0800

    java String Type

--- src/com/jie/java_base/StringExample.java
+++ src/com/jie/java_base/StringExample.java
@@ -2,6 +2,29 @@ package com.jie.java_base;
 
 public class StringExample {
     public static void main(String[] args) {
+//        stringPool();
+        newString();
+    }
 
+    private static void stringLiteral() {
+        String str = "123";
+    }
+
+    private static void stringPool() {
+        String s1 = new String("aaa");
+        String s2 = new String("aaa");
+        System.out.println(s1 == s2);
+
+        String s3 = s1.intern();
+        String s4 = s2.intern();
+        System.out.println(s3 == s4);
+
+        String s5 = "bbb";
+        String s6 = "bbb";
+        System.out.println(s5 == s6);
+    }
+
+    private static void newString() {
+        String str = new String("abc");
     }
 }
