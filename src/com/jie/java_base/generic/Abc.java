211f97938878e3605dc8989059456455d8d1e4fa
何霖 <lin12.he@tcl.com>
2021-12-17 10:51:18 +0800

    can't create wildcard array directly

--- src/com/jie/java_base/generic/Abc.java
+++ src/com/jie/java_base/generic/Abc.java
@@ -1,5 +1,8 @@
 package com.jie.java_base.generic;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 /**
  * can't create wildcard array directly
  **/
@@ -8,5 +11,26 @@ public class Abc<T> {
 //        return new T[5];
 //    }
 
+    T[] createArray(Class<T> clz) {
+        return (T[]) Array.newInstance(clz, 5);
+    }
 
+    @SafeVarargs
+    static <T> T[] asArray(T... objs) {
+        return objs;
+    }
+
+    static <K> K[] pickTwo(K k1, K k2, K k3) {
+        return asArray(k1, k2);
+    }
+
+    public static void main(String[] args) {
+        String[] ss = Abc.asArray("1", "2", "3");
+        Integer[] ii = Abc.asArray(1, 2, 3);
+        System.out.println(Arrays.toString(ss));
+        System.out.println(Arrays.toString(ii));
+
+//        String [] pp = pickTwo("1", "2", "3");
+//        System.out.println(Arrays.toString(pp));
+    }
 }
